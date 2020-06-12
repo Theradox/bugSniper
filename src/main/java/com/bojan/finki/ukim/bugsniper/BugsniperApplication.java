dcdbc3e72bea3818d9763110bf6ca58328c088b4
Theradox <bojan.spasovski26@gmail.com>
2020-06-12 18:42:26 +0200

    Initial commit

---
+++ src/main/java/com/bojan/finki/ukim/bugsniper/BugsniperApplication.java
@@ -0,0 +1,13 @@
+package com.bojan.finki.ukim.bugsniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugsniperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugsniperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bojan/finki/ukim/bugsniper/BugsniperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bojan.finki.ukim.bugsniper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugsniperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
